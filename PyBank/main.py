d264514f8469ebd853209ed68da95f5ee9b3b9f4
unknown <skraus2645@gmail.com>
2019-09-19 15:26:26 -0500

    Results

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,60 @@
 # This is main.py in PyBank
+
+import os
+import csv
+
+filename = input("budget_data.csv")
+month_count = 0
+revenue_total = 0 
+current_month_revenue = 0
+last_month_revenue = 0
+revenue_change = 0
+revenue_changes = []
+months = []
+
+filepath = os.path.join("budget_data.csv")
+with open(filepath, "r", newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    next(csvreader)
+
+
+    for row in csvreader:
+        month_count = month_count + 1
+        months.append(row[0])
+        current_month_revenue = int(row[1])
+        revenue_total = revenue_total + current_month_revenue
+        if month_count > 1:
+            revenue_change = current_month_revenue - last_month_revenue
+            revenue_changes.append(revenue_change)
+        last_month_revenue = current_month_revenue
+
+sum_rev_changes = sum(revenue_changes)
+average_change = sum_rev_changes / (month_count - 1)
+max_change = max(revenue_changes)
+min_change = min(revenue_changes)
+max_month_index = revenue_changes.index(max_change)
+min_month_index = revenue_changes.index(min_change)
+max_month = months[max_month_index]
+min_month = months[min_month_index]
+
+print("Financial Analyst")
+print("----------------------")
+print(f"Total Months: {month_count}")
+print(f"Total Revenue: ${revenue_total}")
+print(f"Average Revenue Change: ${average_change}")
+print(f"Greatest Increase in Revenue: {max_month} (${max_change})")
+print(f"Greatest Decrease in Revenue: {min_month} (${min_change})")
+
+save_file = filename.strip(".csv") + "_results.txt"
+filepath = os.path.join(".", save_file)
+with open(filepath, 'w') as text:
+    text.write("Financial Analyst" + "\n")
+    text.write("-----------------------------" + "\n")
+    text.write(f"Total Months: {month_count}" + "\n")
+    text.write(f"Total Revenue: ${revenue_total}" + "\n")
+    text.write(f"Average Revenue Change: ${average_change}" + "\n")
+    text.write(f"Greatest Increase in Revenue: {max_month} (${max_change})" + "\n")
+    text.write(f"Greatest Decrease in Revenue: {min_month} (${min_change})" + "\n")
+    
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,75 @@
 # This is main.py in PyPoll
+
+import os 
+import csv
+
+candidates = []
+num_votes = 0
+vote_counts = []
+
+filename = input("election_data.csv")
+
+filepath = os.path.join("election_data.csv")
+with open(filepath, 'r', newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    next(csvreader)
+
+
+    for row in csvreader:
+
+        num_votes = num_votes + 1
+
+        candidate = row[2]
+
+        if candidate in candidates:
+            candidate_index = candidates.index(candidate)
+            vote_counts[candidate_index] = vote_counts[candidate_index] + 1
+
+        else:
+            candidates.append(candidate)
+            vote_counts.append(1)
+
+
+percentages = []
+max_votes = vote_counts[0]
+max_index = 0
+
+for count in range(len(candidates)):
+    vote_percentage = vote_counts[count]/num_votes*100
+    percentages.append(vote_percentage)
+    if vote_counts[count] > max_votes:
+        max_votes = vote_counts[count]
+        print(max_votes)
+        max_index = count
+winner = candidates[max_index]
+
+percentages = [round(i,2) for i in percentages]
+
+print("Election Results")
+print("----------------------")
+print(f"Total Votes: {num_votes}")
+print("----------------------")
+for count in range(len(candidates)):
+    print(f"{candidates[count]}: {percentages[count]}% ({vote_counts[count]})")
+print("----------------------")
+print(f"Winner: {winner}")
+print(f"---------------------")
+
+
+
+save_file = filename.strip(".csv") + "_results.txt"
+filepath = os.path.join(".", save_file)
+with open(filepath, 'w') as text:
+    text.write("Election Results" + "\n")
+    text.write("----------------------" + "\n")
+    text.write(f"Total Votes: {num_votes}" + "\n")
+    text.write("----------------------" + "\n")
+    for count in range(len(candidates)):
+        text.write(f"{candidates[count]}: {percentages[count]}% ({vote_counts[count]})" + "\n")
+    text.write("----------------------" + "\n")
+    text.write(f"Winner: {winner}" + "\n")
+    text.write("----------------------" + "\n")
+
+
+
